4258b219611ffe086b62917994c09b2e9b0fb124
William Sciaroni <wsciaroni@protonmail.com>
2023-07-04 22:32:41 +0000

    Bump CMake version to 0.0.2 and require 0.0.1 of proj B

--- cmake/IncludeProjB.cmake
+++ cmake/IncludeProjB.cmake
@@ -4,7 +4,7 @@ macro(include_proj_b)
     FetchContent_Declare(
         ProjB
         GIT_REPOSITORY https://github.com/wsciaroni/projB.git
-        GIT_TAG master
+        GIT_TAG 0.0.1
     )
 
     # Check to see if googletest has already been populated so we only populate it once
